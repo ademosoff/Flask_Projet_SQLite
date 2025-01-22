c28f48f3f2ade52983aed03e2e881b21befba0ba
ademosoff <adalebrazi@gmail.com>
2025-01-22 15:53:00 +0100

    Update create_db2.py

--- create_db2.py
+++ create_db2.py
@@ -19,23 +19,5 @@ cur.execute("INSERT INTO livres (titre, auteur, genre, date_publication, est_dis
 cur.execute("INSERT INTO livres (titre, auteur, genre, date_publication, est_disponible) VALUES (?, ?, ?, ?, 1)",
             ('Les Misérables', 'Victor Hugo', 'Roman', '1862-01-01'))
 
-# Ajout d'autres entrées à la table "livres"
-cur.execute("INSERT INTO livres (titre, auteur, genre, date_publication, est_disponible) VALUES (?, ?, ?, ?, 1)",
-            ('DUPONT', 'Emilie', '123, Rue des Lilas, 75001 Paris', None))
-cur.execute("INSERT INTO livres (titre, auteur, genre, date_publication, est_disponible) VALUES (?, ?, ?, ?, 1)",
-            ('LEROUX', 'Lucas', '456, Avenue du Soleil, 31000 Toulouse', None))
-cur.execute("INSERT INTO livres (titre, auteur, genre, date_publication, est_disponible) VALUES (?, ?, ?, ?, 1)",
-            ('MARTIN', 'Amandine', '789, Rue des Érables, 69002 Lyon', None))
-cur.execute("INSERT INTO livres (titre, auteur, genre, date_publication, est_disponible) VALUES (?, ?, ?, ?, 1)",
-            ('TREMBLAY', 'Antoine', '1010, Boulevard de la Mer, 13008 Marseille', None))
-cur.execute("INSERT INTO livres (titre, auteur, genre, date_publication, est_disponible) VALUES (?, ?, ?, ?, 1)",
-            ('LAMBERT', 'Sarah', '222, Avenue de la Liberté, 59000 Lille', None))
-cur.execute("INSERT INTO livres (titre, auteur, genre, date_publication, est_disponible) VALUES (?, ?, ?, ?, 1)",
-            ('GAGNON', 'Nicolas', '456, Boulevard des Cerisiers, 69003 Lyon', None))
-cur.execute("INSERT INTO livres (titre, auteur, genre, date_publication, est_disponible) VALUES (?, ?, ?, ?, 1)",
-            ('DUBOIS', 'Charlotte', '789, Rue des Roses, 13005 Marseille', None))
-cur.execute("INSERT INTO livres (titre, auteur, genre, date_publication, est_disponible) VALUES (?, ?, ?, ?, 1)",
-            ('LEFEVRE', 'Thomas', '333, Rue de la Paix, 75002 Paris', None))
-
 connection.commit()
 connection.close()
